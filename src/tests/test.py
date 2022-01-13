aa615a0c3a3f9a100de17c1247e73a289e8c8783
omartin2010 <omartin@live.ca>
2022-01-12 23:22:32 -0500

    changed to use math in contract

--- src/tests/test.py
+++ src/tests/test.py
@@ -4,7 +4,9 @@ from univ3.UNI_v3_support_funcs import get_eth_usd, univ3_position #  univ3_pool
 pos = univ3_position('166439')
 print(f'get_eth_usd = {get_eth_usd()}')
 print(f'unclaimed = {pos.get_uncollected_fees()}')
-
+# pos._calculate_uncollected_fees_2()
+# print(f'tok0 = ={pos._last_feetoken0}')
+# print(f'tok1 = ={pos._last_feetoken1}')
 # print(pos.describe_position())
 
 output = json.dumps(pos.describe_position())
